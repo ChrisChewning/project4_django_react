5a40dcf17756d8635180afeb54da805b993a9847
Chris <chewning.cl@gmail.com>
2018-08-30 14:04:52 -0500

    heroku deployment Thursday 4

--- showtime_django/settings.py
+++ showtime_django/settings.py
@@ -105,26 +105,26 @@ WSGI_APPLICATION = 'showtime_django.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#         'NAME': 'd9dgvphuhii6go',
-#         'USER': 'zabaulnlvtvfmn',
-#         'PASSWORD': '13825af582c06951a63f932c88496fcbaa78648a34415170afc771f153172cc8',
-#         'HOST': 'ec2-54-225-92-1.compute-1.amazonaws.com',
-#         'PORT': '5432',
-#     }
-# }
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'showtime',
-        'USER': 'showtimeuser',
-        'PASSWORD': 'showtime',
-        'HOST': 'localhost'
+        'NAME': 'd9dgvphuhii6go',
+        'USER': 'zabaulnlvtvfmn',
+        'PASSWORD': '13825af582c06951a63f932c88496fcbaa78648a34415170afc771f153172cc8',
+        'HOST': 'ec2-54-225-92-1.compute-1.amazonaws.com',
+        'PORT': '5432',
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'showtime',
+#         'USER': 'showtimeuser',
+#         'PASSWORD': 'showtime',
+#         'HOST': 'localhost'
+#     }
+# }
 
 
 
